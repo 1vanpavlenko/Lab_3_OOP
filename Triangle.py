1fd936b354618e6c8da2e38e004c823d2d8a1983
Ivan Pavlenko <ivan_pavlenko@knu.ua>
2024-02-27 10:02:43 +0200

    updated

--- Triangle.py
+++ Triangle.py
@@ -1,4 +1,5 @@
 import turtle
+from math import sin, cos, pi, radians
 
 
 class Triangle:
@@ -12,18 +13,51 @@ class Triangle:
 
         self.position = (0, 0)
 
+        self.rotation = 0
+
     def set_position(self, x, y):
         self.position = (x, y)
+
+    def move(self, dx, dy):
+        self.position = (self.position[0] + dx, self.position[1] + dy)
+
+    def set_rotation(self, rotation):
+        self.rotation = rotation
+
+    def set_rotation_degree(self, degree):
+        self.rotation = radians(degree)
+
+    def rotation_matrix(self):
+        fi = self.rotation
+
+        fi_matrix = [[cos(fi), -sin(fi)],
+                     [sin(fi), cos(fi)]]
+
+        return fi_matrix
+
+    @staticmethod
+    def mult_matrix_vector(m, v):
+        turned_vertex = [0, 0]
+
+        turned_vertex[0] = m[0][0] * v[0] + m[0][1] * v[1]
+        turned_vertex[1] = m[1][0] * v[0] + m[1][1] * v[1]
+
+        return turned_vertex
 
     def set_color(self, color):
         self.color = color
 
     def calc_abs_pos(self):
-        v1 = (self.position[0] + self.vertex1[0],
-              self.position[1] + self.vertex1[1])
+        m = self.rotation_matrix()
 
-        v2 = (self.position[0] + self.vertex2[0],
-              self.position[1] + self.vertex2[1])
+        turned_vertex1 = self.mult_matrix_vector(m, self.vertex1)
+        turned_vertex2 = self.mult_matrix_vector(m, self.vertex2)
+
+        v1 = (self.position[0] + turned_vertex1[0],
+              self.position[1] + turned_vertex1[1])
+
+        v2 = (self.position[0] + turned_vertex2[0],
+              self.position[1] + turned_vertex2[1])
 
         return v1, v2
 
@@ -31,8 +65,6 @@ class Triangle:
         v1, v2 = self.calc_abs_pos()
 
         turtle.color(self.color)
-
-        turtle.speed(0.5)
 
         turtle.up()
 
--- main.py
+++ main.py
@@ -4,30 +4,42 @@ import random
 
 triangles = []
 
-for i in range(100):
-    x1, y1 = random.randint(-100, 100), random.randint(-100, 100)
-    x2, y2 = random.randint(-100, 100), random.randint(-100, 100)
+# for i in range(100):
+#     x1, y1 = random.randint(-100, 100), random.randint(-100, 100)
+#     x2, y2 = random.randint(-100, 100), random.randint(-100, 100)
+#
+#     t = Triangle(x1, y1, x2, y2)
+#
+#     x, y = random.randint(-100, 100), random.randint(-100, 100)
+#
+#     t.set_position(x, y)
+#
+#     r, g, b = (random.randint(0, 255) for _ in range(3))
+#
+#     color = r << 16 | g << 8 | b
+#
+#     hex_color = str(hex(color))[2:]
+#
+#     if len(hex_color) < 6:
+#         hex_color = (6 - len(hex_color)) * '0' + hex_color
+#
+#     t.set_color('#' + hex_color)
+#
+#     triangles.append(t)
+#
+# for triangle in triangles:
+#     triangle.draw()
 
-    t = Triangle(x1, y1, x2, y2)
+turtle.speed(0)
 
-    x, y = random.randint(-100, 100), random.randint(-100, 100)
+triangle = Triangle(100, 100, 100, 0)
+triangle.draw()
 
-    t.set_position(x, y)
-
-    r, g, b = (random.randint(0, 255) for _ in range(3))
-
-    color = r << 16 | g << 8 | b
-
-    hex_color = str(hex(color))[2:]
-
-    if len(hex_color) < 6:
-        hex_color = (6 - len(hex_color)) * '0' + hex_color
-
-    t.set_color('#' + hex_color)
-
-    triangles.append(t)
-
-for triangle in triangles:
+for degree in range(3, 363, 3):
+    triangle.set_rotation_degree(degree)
     triangle.draw()
+    triangle.move(0, -1 / 3)
+
+    turtle.clear() if degree < 360 else turtle
 
 turtle.mainloop()
